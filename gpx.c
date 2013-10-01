fa3179013f8e610cd42d0d3753daf407e9dddc15
Eric Fischer <enf@pobox.com>
2013-10-01 15:42:26 -0700

    Store points into a buffer

--- gpx.c
+++ gpx.c
@@ -10,11 +10,38 @@
 #include <math.h>
 #include <time.h>
 
+struct point {
+	float lat;
+	float lon;
+};
+
+struct point *points = NULL;
+int npoints = 0;
+int npalloc = 0;
+
+#define FOOT .00000274
+#define BUCKET (100 * FOOT)
+
 // boilerplate from
 // http://marcomaggi.github.io/docs/expat.html#overview-intro
 // Copyright 1999, Clark Cooper
 
 #define BUFFSIZE        8192
+
+void point(double lat, double lon) {
+	if (npoints >= npalloc) {
+		npalloc = npalloc * 2 + 1024;
+		points = realloc(points, npalloc * sizeof(struct point));
+		if (points == NULL) {
+			perror("realloc");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	points[npoints].lat = lat;
+	points[npoints].lon = lon;
+	npoints++;
+}
 
 static void XMLCALL start(void *data, const char *element, const char **attribute) {
 	if (strcmp(element, "trkpt") == 0) {
@@ -30,9 +57,9 @@ static void XMLCALL start(void *data, const char *element, const char **attribut
 			}
 		}
 
-		printf("%f,%f\n", lat, lon);
+		point(lat, lon);
 	} else if (strcmp(element, "trk") == 0) {
-		printf("--\n");
+		point(0, 0);
 	}
 }
 
@@ -77,6 +104,7 @@ int main(int argc, char *argv[]) {
 
 		for (i = 1; i < argc; i++) {
 			FILE *f = fopen(argv[i], "r");
+			fprintf(stderr, "%s\n", argv[i]);
 			if (f == NULL) {
 				perror(argv[i]);
 				exit(EXIT_FAILURE);
@@ -86,4 +114,6 @@ int main(int argc, char *argv[]) {
 			fclose(f);
 		}
 	}
+	printf("got %d points\n", npoints);
+	exit(EXIT_SUCCESS);
 }
